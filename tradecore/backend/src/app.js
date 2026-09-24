9618b6c1e6ddd66b51c4b37cdb04aa276c024ae0
IsiaqOladayo <oladayoisiaq@gmail.com>
2026-09-24 18:43:12 +0100

    Add amplify live url to origin

--- tradecore/backend/src/app.js
+++ tradecore/backend/src/app.js
@@ -18,6 +18,7 @@ app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3001',
   credentials: true,
+  https://develop.dpqtxdawh7h1c.amplifyapp.com/
 }));
 
 // ── Rate limiting ─────────────────────────────────────────────────
